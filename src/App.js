ec179ce25f3c2056a9eabe5c38c3acc4dff944e9
Raghul <36655257+raghul7@users.noreply.github.com>
2025-04-26 04:59:31 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
-import {createBrowserRouter , RouterProvider } from 'react-router'
-import LoginPage from "../src/components/Login";
-import SignUp from "../src/components/SignUp";
-
+import Dashboard from './Components/Dashboard';
+import PatienceAdmission from './Components/PatienceAdmission';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 
 function App() {
-const routes = createBrowserRouter([
-  {
-    path:"/",
-    element:<LoginPage />
-  },
-  {
-    path:"/signup",
-    element:<SignUp />
-  }
-])
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      Component: Login,
+    },
+    {
+      path: "/patient-admission",
+      Component: PatienceAdmission,
+    },
+    {
+      path: "/dashboard",
+      Component: Dashboard,
+    },
+    {
+      path: "/register",
+      Component: SignUp,
+    }
 
-  return (
-    <>
-    <RouterProvider router={routes} />
-    </>
-    
-    
-  );
+  ]);
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <Routes>
+  //         <Route path="/" element={<Dashboard />} />
+  //       </Routes>
+  //     </header>
+  //   </div>
+  // );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
